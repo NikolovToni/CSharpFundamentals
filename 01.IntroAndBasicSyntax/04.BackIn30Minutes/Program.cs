e512553de6e13ecf6e1b968cfbb96c9f30633555
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-03 01:36:26 +0300

    solved the fourth task

--- 01.IntroAndBasicSyntax/04.BackIn30Minutes/Program.cs
+++ 01.IntroAndBasicSyntax/04.BackIn30Minutes/Program.cs
@@ -6,7 +6,28 @@ namespace _04.BackIn30Minutes
     {
         static void Main(string[] args)
         {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
 
+            if (minutes + 30 > 59)
+            {
+                minutes = (minutes + 30) - 60;
+                hours++;
+            }
+            else
+            {
+                minutes += 30;
+            }
+
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            
+
+            //Console.WriteLine($"{hours}:{minutes}");
+            Console.WriteLine("{0:00}:{1:00}", hours, minutes);
         }
     }
 }
