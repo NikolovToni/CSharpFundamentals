45fb5fd6ba3ae16a66421cfca355c01a2f08656c
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-03 02:01:32 +0300

    create the 7th project

---
+++ 01.IntroAndBasicSyntax/07.SumOfOddNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i+1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
