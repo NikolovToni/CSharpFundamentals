2328896e2a777dad5127f000f2371296ed59b6f4
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-03 01:49:08 +0300

    solved the 5th task

--- 01.IntroAndBasicSyntax/05.MonthPrinter/Program.cs
+++ 01.IntroAndBasicSyntax/05.MonthPrinter/Program.cs
@@ -6,7 +6,32 @@ namespace _05.MonthPrinter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int monthNumber = int.Parse(Console.ReadLine());
+
+            string[] month = new string[]
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+
+            if (monthNumber < 1 || monthNumber > 12)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine(month[monthNumber-1]);
+            }
         }
     }
 }
