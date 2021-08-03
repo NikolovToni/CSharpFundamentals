9f4f47e3c25701633c85251bbdd43ec9f3136902
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-03 16:51:35 +0300

    solved the 8th task

--- 01.IntroAndBasicSyntax/08.MultiplicationTable/Program.cs
+++ 01.IntroAndBasicSyntax/08.MultiplicationTable/Program.cs
@@ -6,7 +6,12 @@ namespace _08.MultiplicationTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
         }
     }
 }
