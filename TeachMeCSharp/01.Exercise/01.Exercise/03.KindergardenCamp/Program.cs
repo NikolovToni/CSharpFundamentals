9c893c83107b05bec0d40c55338646c3ca0366de
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 18:05:48 +0300

    creatking the KindergardenCamp project

---
+++ TeachMeCSharp/01.Exercise/01.Exercise/03.KindergardenCamp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.KindergardenCamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
