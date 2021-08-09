fdf58852e8fcdd3cf86f02a6d97559e2b152dd47
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 20:51:06 +0300

    Task solved

--- TeachMeCSharp/02.Loops/05.UserLogin/Program.cs
+++ TeachMeCSharp/02.Loops/05.UserLogin/Program.cs
@@ -6,7 +6,20 @@ namespace _05.UserLogin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string user = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string inputPassword = Console.ReadLine();
+            while (inputPassword != password)
+            {
+                Console.WriteLine("Wrong password!");
+                inputPassword = Console.ReadLine();
+                if (inputPassword == password)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Welcome, {user}");
         }
     }
 }
