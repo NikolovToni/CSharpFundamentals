6d80bbc20e522a49148e05b338e48653bb84e46b
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 20:31:21 +0300

    task solved

--- TeachMeCSharp/02.Loops/03.LetterDecay/Program.cs
+++ TeachMeCSharp/02.Loops/03.LetterDecay/Program.cs
@@ -6,7 +6,12 @@ namespace _03.LetterDecay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
         }
     }
 }
