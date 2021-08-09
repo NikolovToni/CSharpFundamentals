2b0d97d287463c16b79b719cb49995e47a4f1744
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 21:26:21 +0300

    Task solved

--- TeachMeCSharp/02.Loops/07.Clock/Program.cs
+++ TeachMeCSharp/02.Loops/07.Clock/Program.cs
@@ -6,7 +6,13 @@ namespace _07.Clock
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int j = 0; j <= 59; j++)
+                {
+                    Console.WriteLine($"{i:D2}:{j:D2}");
+                }
+            }
         }
     }
 }
