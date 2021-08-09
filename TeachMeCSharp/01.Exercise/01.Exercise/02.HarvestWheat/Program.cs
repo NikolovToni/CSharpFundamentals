db4414152fc810906f4abcbbd714be2f59446f0e
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 18:03:00 +0300

    solving the task

--- TeachMeCSharp/01.Exercise/01.Exercise/02.HarvestWheat/Program.cs
+++ TeachMeCSharp/01.Exercise/01.Exercise/02.HarvestWheat/Program.cs
@@ -6,7 +6,23 @@ namespace _02.HarvestWheat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int area = int.Parse(Console.ReadLine());
+            double grain = double.Parse(Console.ReadLine());
+            int needFlour = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double totalWheat = area * grain;
+            double totalFlour = 0.4 * totalWheat / 2.5;
+
+            if (totalFlour < needFlour)
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(needFlour - totalFlour)} kilos needed");
+            }
+            else
+            {
+                Console.WriteLine($"Good harvest this year! Total flour: {Math.Floor(totalWheat)} kilos.");
+                Console.WriteLine($"{Math.Ceiling(totalFlour - needFlour)} kilos left -> {Math.Ceiling((totalFlour - needFlour) / workers)}");
+            }
         }
     }
 }
