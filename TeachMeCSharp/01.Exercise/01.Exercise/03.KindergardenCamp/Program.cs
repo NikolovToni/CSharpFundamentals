7c52245eb90cbf65877b04f5f9581bd54ae377ca
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 19:09:56 +0300

    solved the KindergardenCamp task

--- TeachMeCSharp/01.Exercise/01.Exercise/03.KindergardenCamp/Program.cs
+++ TeachMeCSharp/01.Exercise/01.Exercise/03.KindergardenCamp/Program.cs
@@ -6,7 +6,111 @@ namespace _03.KindergardenCamp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string season = Console.ReadLine();
+            string groupType = Console.ReadLine();
+            int children = int.Parse(Console.ReadLine());
+            int nightStay = int.Parse(Console.ReadLine());
+            string kindOfSport = string.Empty;
+
+            double nightPrice = 0;
+
+            switch (groupType)
+            {
+                case "boys":
+                case "girls":
+                    if (season == "winter")
+                    {
+                        nightPrice = nightStay * 9.6;
+
+                        if (groupType == "boys")
+                        {
+                            kindOfSport = "Box";
+                        }
+                        else if (groupType == "girls")
+                        {
+                            kindOfSport = "Gymnastics";
+                        }
+                        else
+                        {
+                            kindOfSport = "Snowboard";
+                        }
+                    }
+                    else if (season == "spring")
+                    {
+                        nightPrice = nightStay * 7.2;
+
+                        if (groupType == "boys")
+                        {
+                            kindOfSport = "Tennis";
+                        }
+                        else if (groupType == "girls")
+                        {
+                            kindOfSport = "Aerobics";
+                        }
+                        else
+                        {
+                            kindOfSport = "Cycling";
+                        }
+                    }
+                    else
+                    {
+                        nightPrice = nightStay * 15;
+
+                        if (groupType == "boys")
+                        {
+                            kindOfSport = "Football";
+                        }
+                        else if (groupType == "girls")
+                        {
+                            kindOfSport = "Volleyball";
+                        }
+                        else
+                        {
+                            kindOfSport = "Swimming";
+                        }
+                    }
+                    break;
+                case "mix":
+                    if (season == "winter")
+                    {
+                        nightPrice = nightStay * 10;
+                        kindOfSport = "Snowboard";
+                    }
+                    else if (season == " spring")
+                    {
+                        nightPrice = nightStay * 9.5;
+                        kindOfSport = "Cycling";
+                    }
+                    else
+                    {
+                        nightPrice = nightStay * 20;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            double totalPrice = children * nightPrice;
+            double discount = 0;
+
+            if (children >= 50)
+            {
+                discount = totalPrice / 2;
+            }
+            else if (children >= 20 && children < 50)
+            {
+                discount = totalPrice - (totalPrice * 0.15);
+            }
+            else if (children >= 10 && children < 20)
+            {
+                discount = totalPrice - (totalPrice * 0.1);
+            }
+            else
+            {
+                discount = totalPrice;
+            }
+
+            Console.WriteLine($"{kindOfSport} {discount:F2} BGN");
         }
     }
 }
