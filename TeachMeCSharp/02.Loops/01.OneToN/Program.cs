217cdd7f2b9c1518e5149f69dcbf7258db19eb8a
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 20:23:01 +0300

    solved the task

--- TeachMeCSharp/02.Loops/01.OneToN/Program.cs
+++ TeachMeCSharp/02.Loops/01.OneToN/Program.cs
@@ -6,7 +6,12 @@ namespace _01.OneToN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i+=2)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
