d65453c0f9b853980a980df376ef52dca0a4c9a5
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 21:07:17 +0300

    Task solved

--- TeachMeCSharp/02.Loops/06.ATM/Program.cs
+++ TeachMeCSharp/02.Loops/06.ATM/Program.cs
@@ -6,7 +6,27 @@ namespace _06.ATM
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            double import;
+            double total = 0;
+            for (int i = 0; i < n; i++)
+            {
+                import = double.Parse(Console.ReadLine());
+
+                if (import > 0)
+                {
+                    Console.WriteLine($"Increase: {import}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                total += import;
+            }
+
+            Console.WriteLine($"Total: {total}");
         }
     }
 }
