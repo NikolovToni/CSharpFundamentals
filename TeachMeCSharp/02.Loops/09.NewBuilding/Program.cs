5de9d75854b9795aef7ee15548fb6a48b42a287e
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 22:27:16 +0300

    Task solved

--- TeachMeCSharp/02.Loops/09.NewBuilding/Program.cs
+++ TeachMeCSharp/02.Loops/09.NewBuilding/Program.cs
@@ -6,7 +6,34 @@ namespace _09.NewBuilding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            int lastFloor = 0;
+            for (int i = floors; i >= 1; i--)
+            {
+                if (lastFloor < i)
+                {
+                    lastFloor = i;
+                }
+
+                for (int j = 0; j < rooms; j++)
+                {
+                    if (i % 2 == 0 && i != lastFloor)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else if (i % 2 != 0 && i != lastFloor)
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                    else
+                    {
+                        Console.Write($"L{i}{j} ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
