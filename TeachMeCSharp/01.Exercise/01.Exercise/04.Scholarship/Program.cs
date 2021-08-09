ccccc1eb7f468c25a955b7809882290203eb0232
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-09 20:14:32 +0300

    solved the 04.Scholarship task

--- TeachMeCSharp/01.Exercise/01.Exercise/04.Scholarship/Program.cs
+++ TeachMeCSharp/01.Exercise/01.Exercise/04.Scholarship/Program.cs
@@ -6,7 +6,40 @@ namespace _04.Scholarship
     {
         static void Main(string[] args)
         {
+            double income = double.Parse(Console.ReadLine());
+            double avgGrade = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
 
+            double amounthExellent = 0;
+            double amounthSocial = 0;
+            bool canIt = true;
+
+            if (avgGrade >= 5.5)
+            {
+                amounthExellent = avgGrade * 25;
+            }
+
+            if (income < minSalary && avgGrade >= 4.5)
+            {
+                amounthSocial = minSalary * 0.35;
+            }
+            else
+            {
+                canIt = false;
+            }
+
+            if (amounthExellent > amounthSocial && canIt == true)
+            {
+                Console.WriteLine($"You get a scholarship for exellent result {Math.Floor(amounthExellent)} BGN");
+            }
+            else if (amounthExellent < amounthSocial && canIt == true)
+            {
+                Console.WriteLine($"You get a Social scholarship {Math.Floor(amounthSocial)} BGN");
+            }
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
         }
     }
 }
