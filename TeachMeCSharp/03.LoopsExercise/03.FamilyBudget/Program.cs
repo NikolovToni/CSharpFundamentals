373ce8b2814c3a734cdef67d41b50eab1c0581d8
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-11 02:11:41 +0300

    In process of soling the Others

--- TeachMeCSharp/03.LoopsExercise/03.FamilyBudget/Program.cs
+++ TeachMeCSharp/03.LoopsExercise/03.FamilyBudget/Program.cs
@@ -6,7 +6,36 @@ namespace _03.FamilyBudget
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int months = int.Parse(Console.ReadLine());
+            double electricity = 0;
+            double water = 20;
+            double internet = 15;
+            double others = 0;
+
+            double electricityPrice = 0;
+            double waterPrice = 0;
+            double internetPrice = 0;
+            double othersPrice = 0;
+
+            for (int i = 0; i < months; i++)
+            {
+                electricity = double.Parse(Console.ReadLine());
+                electricityPrice += electricity;
+                others = electricity + water + internet + ((electricity + water + internet) * 0.2);
+            }
+
+            for (int i = 0; i < months; i++)
+            {
+                waterPrice += water;
+                internetPrice += internet;
+                othersPrice += others;
+            }
+
+            Console.WriteLine($"Electricity: {electricityPrice:F2} BGN");
+            Console.WriteLine($"Water: {waterPrice:F2} BGN");
+            Console.WriteLine($"internet: {internetPrice:F2} BGN");
+            Console.WriteLine($"Others: {othersPrice:F2} BGN");
+            Console.WriteLine($"Average: {(electricityPrice + waterPrice + internetPrice + othersPrice) / months:F2} BGN");
         }
     }
 }
