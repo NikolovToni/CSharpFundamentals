eac9285549ab466fe4156780fe5a456d4134d8d1
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-11 02:29:53 +0300

    Making a For loop

--- TeachMeCSharp/03.LoopsExercise/05.RhombusOfAsterisks/Program.cs
+++ TeachMeCSharp/03.LoopsExercise/05.RhombusOfAsterisks/Program.cs
@@ -6,7 +6,17 @@ namespace _05.RhombusOfAsterisks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
