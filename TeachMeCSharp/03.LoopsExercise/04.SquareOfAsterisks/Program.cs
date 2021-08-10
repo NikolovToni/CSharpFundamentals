d1bda6b0a4b62198c9d25b194a1f24541e047809
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-11 02:16:38 +0300

    Task solved

--- TeachMeCSharp/03.LoopsExercise/04.SquareOfAsterisks/Program.cs
+++ TeachMeCSharp/03.LoopsExercise/04.SquareOfAsterisks/Program.cs
@@ -6,7 +6,16 @@ namespace _04.SquareOfAsterisks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
