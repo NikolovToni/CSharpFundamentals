a631457acf668d2b4c221703bc1a38b5a08efbd6
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-11 01:25:11 +0300

    Task solved

--- TeachMeCSharp/03.LoopsExercise/01.AvgNumber/Program.cs
+++ TeachMeCSharp/03.LoopsExercise/01.AvgNumber/Program.cs
@@ -6,7 +6,16 @@ namespace _01.AvgNumber
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            double number;
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                number = double.Parse(Console.ReadLine());
+                sum += number;
+            }
 
+            Console.WriteLine($"{sum / n:F2}");
         }
     }
 }
