38b0f7582a6b3c9c2ef2eaec14d7fe0a4cc910ae
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-15 12:05:21 +0300

    Task solved

--- TeachMeCSharp/04.ArraysExercise/01.DayOfWeek/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/01.DayOfWeek/Program.cs
@@ -6,7 +6,26 @@ namespace _01.DayOfWeek
     {
         static void Main(string[] args)
         {
-            
+            int dayNumber = int.Parse(Console.ReadLine());
+
+            string[] dayOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (dayNumber >= 1 && dayNumber <=7)
+            {
+                Console.WriteLine(dayOfWeek[dayNumber - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
 }
