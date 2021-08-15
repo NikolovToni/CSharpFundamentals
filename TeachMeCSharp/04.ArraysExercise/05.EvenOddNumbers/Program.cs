586b90440370691ec24dd19c5116fd26ee51b06e
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-15 12:50:41 +0300

    Task solved

--- TeachMeCSharp/04.ArraysExercise/05.EvenOddNumbers/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/05.EvenOddNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05.EvenOddNumbers
 {
@@ -6,7 +7,36 @@ namespace _05.EvenOddNumbers
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    sumEven += arr[i];
+                }
+                else
+                {
+                    sumOdd += arr[i];
+                }
+            }
+
+            int difference = sumEven - sumOdd;
+
+            if (difference > 0)
+            {
+                Console.WriteLine(difference);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
