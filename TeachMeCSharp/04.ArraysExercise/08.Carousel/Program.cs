4d72c60d1246cc578edeac6df3badb6737f856c3
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-15 19:11:20 +0300

    Task solved

--- TeachMeCSharp/04.ArraysExercise/08.Carousel/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/08.Carousel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08.Carousel
 {
@@ -6,7 +7,24 @@ namespace _08.Carousel
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = int.Parse(Console.ReadLine());
 
+            int temp = arr[0];
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', arr));
         }
     }
 }
