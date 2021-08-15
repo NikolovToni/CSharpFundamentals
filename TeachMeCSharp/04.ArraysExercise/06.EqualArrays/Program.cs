aebc38efb9e2bb8b33050d9e62b5c2ace431ed5f
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-15 13:58:40 +0300

    Task solved

--- TeachMeCSharp/04.ArraysExercise/06.EqualArrays/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/06.EqualArrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06.EqualArrays
 {
@@ -6,7 +7,42 @@ namespace _06.EqualArrays
     {
         static void Main(string[] args)
         {
+            int[] firstArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            bool areEqual = false;
+            int sum = 0;
+            int index = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] == secondArray[i])
+                {
+                    sum += firstArray[i];
+                    areEqual = true;
+                }
+                else
+                {
+                    index = firstArray[i];
+                    areEqual = false;
+                    break;
+                }
+            }
+
+            if (areEqual)
+            {
+                Console.WriteLine($"Equal arrays, Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Not equal. Difference at index {index - 1}");
+            }
         }
     }
 }
