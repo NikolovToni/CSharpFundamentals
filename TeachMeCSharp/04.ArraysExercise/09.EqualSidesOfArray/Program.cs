0bb5f2c67f170ca93527ca43c43d56891aec2ec6
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-15 19:32:42 +0300

    Task solved

--- TeachMeCSharp/04.ArraysExercise/09.EqualSidesOfArray/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/09.EqualSidesOfArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _09.EqualSidesOfArray
 {
@@ -6,7 +7,35 @@ namespace _09.EqualSidesOfArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int leftSide = 0;
+            int rightSide = 0;
+
+            for (int i = 0; i < arr[arr.Length / 2 - 1]; i++) 
+            {
+                //Console.Write($"{arr[i]} ");
+                leftSide += arr[i];
+            }
+
+            //Console.WriteLine();
+            for (int i = arr.Length / 2; i < arr.Length ; i++)
+            {
+                //Console.Write($"{arr[i]} ");
+                rightSide += arr[i];
+            }
+
+            if (leftSide == rightSide)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
         }
     }
 }
