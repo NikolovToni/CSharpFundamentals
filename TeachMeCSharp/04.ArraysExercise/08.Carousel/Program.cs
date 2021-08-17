00baec29250c583f5a852e5a4aac434d98cd853f
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-17 15:45:26 +0300

    Initial commit

--- TeachMeCSharp/04.ArraysExercise/08.Carousel/Program.cs
+++ TeachMeCSharp/04.ArraysExercise/08.Carousel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08.Carousel
 {
@@ -6,7 +7,26 @@ namespace _08.Carousel
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            int n = int.Parse(Console.ReadLine());
+            int temp;
+
+            while (n>0)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    temp = arr[0];
+                    arr[0] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    n--;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', arr));
         }
     }
 }
