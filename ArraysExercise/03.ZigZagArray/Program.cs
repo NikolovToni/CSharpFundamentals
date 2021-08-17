1b14558a155f89378ec9c0f0da4ddcdef32d9434
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-17 16:20:33 +0300

    Task solves

---
+++ ArraysExercise/03.ZigZagArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.ZigZagArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = input[0];
+                    secondArray[i] = input[1];
+                }
+                else
+                {
+                    secondArray[i] = input[0];
+                    firstArray[i] = input[1];
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', firstArray));
+            Console.WriteLine(string.Join(' ', secondArray));
+        }
+    }
+}
