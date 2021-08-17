60c9d3cd38c3f6d3ea38306eeb938008093e2832
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-17 15:57:33 +0300

    Task solved

---
+++ ArraysExercise/02.CommonElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string[] secondArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j < secondArray.Length; j++)
+                {
+                    if (secondArray[i] == firstArray[j])
+                    {
+                        Console.Write($"{secondArray[i]} ");
+                    }
+
+                    // Console.WriteLine($"{firstArray[i]} {secondArray[j]}");
+                }
+            }
+        }
+    }
+}
