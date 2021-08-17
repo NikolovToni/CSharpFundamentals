98929af726c88e6db9bca6a03c4dd9127274d8cc
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-17 16:33:30 +0300

    Task solved

---
+++ ArraysExercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int temp = arr[0];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
