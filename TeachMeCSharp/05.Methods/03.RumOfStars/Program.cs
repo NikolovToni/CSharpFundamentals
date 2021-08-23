c0168c55c4af533dc080b66f2c71639dc2de6f49
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-23 22:44:27 +0300

    Create RumOfStars

---
+++ TeachMeCSharp/05.Methods/03.RumOfStars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.RumOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
