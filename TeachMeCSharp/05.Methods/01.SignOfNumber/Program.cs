f1a95b9e0bd1f190205a029b8213dfbc01ee6a82
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-23 19:54:13 +0300

    Task solved

--- TeachMeCSharp/05.Methods/01.SignOfNumber/Program.cs
+++ TeachMeCSharp/05.Methods/01.SignOfNumber/Program.cs
@@ -6,7 +6,24 @@ namespace _01.SignOfNumber
     {
         static void Main(string[] args)
         {
+            int number = int.Parse(Console.ReadLine());
+            GetSignOfNumber(number);
+        }
 
+        static void GetSignOfNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("zero");
+            }
         }
     }
 }
