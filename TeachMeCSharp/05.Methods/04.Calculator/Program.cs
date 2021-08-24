4461cb141accd7655eeef816a4fc2fe532046118
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-24 19:26:58 +0300

    Task solved

--- TeachMeCSharp/05.Methods/04.Calculator/Program.cs
+++ TeachMeCSharp/05.Methods/04.Calculator/Program.cs
@@ -6,7 +6,50 @@ namespace _04.Calculator
     {
         static void Main(string[] args)
         {
+            string command = Console.ReadLine();
 
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+
+            switch (command)
+            {
+                case "add":
+                    Console.WriteLine(Add(firstNum, secondNum));
+                    break;
+                case "subtract":
+                    Console.WriteLine(Subtract(firstNum, secondNum));
+                    break;
+                case "multiply":
+                    Console.WriteLine(Multiply(firstNum, secondNum));
+                    break;
+                case "divide":
+                    Console.WriteLine(Divide(firstNum, secondNum));
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Divide(double a, double b)
+        {
+            return a / b;
         }
     }
 }
