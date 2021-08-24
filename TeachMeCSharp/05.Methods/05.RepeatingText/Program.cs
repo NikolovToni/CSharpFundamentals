33799ed603474f4ea2679a07e36ca1d6df84e2c6
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-24 19:35:15 +0300

    Task solved

--- TeachMeCSharp/05.Methods/05.RepeatingText/Program.cs
+++ TeachMeCSharp/05.Methods/05.RepeatingText/Program.cs
@@ -6,7 +6,21 @@ namespace _05.RepeatingText
     {
         static void Main(string[] args)
         {
+            string word = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
 
+            Repeating(word, count);
+        }
+
+        static void Repeating(string word, int count)
+        {
+            string temp = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                temp += word;
+            }
+
+            Console.WriteLine(temp);
         }
     }
 }
