04049f1c864941217edbb3d27a21524215415793
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-24 17:43:21 +0300

    Creating for loops

--- TeachMeCSharp/05.Methods/03.RumOfStars/Program.cs
+++ TeachMeCSharp/05.Methods/03.RumOfStars/Program.cs
@@ -6,7 +6,25 @@ namespace _03.RumOfStars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            RumOfStars(n);
+        }
+
+        static void RumOfStars(int num)
+        {
+            for (int row = 0; row < num; row++)
+            {
+                for (int col = 1; col <= num - row; col--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int col = 1; col < row; col++)
+                {
+                    Console.WriteLine(" *");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
