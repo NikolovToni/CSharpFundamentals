f8495f264a42c6586d1968b7c5a3c9777f2430a9
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-24 19:42:51 +0300

    Task solved

--- TeachMeCSharp/05.Methods/06.Carousel/Program.cs
+++ TeachMeCSharp/05.Methods/06.Carousel/Program.cs
@@ -6,7 +6,25 @@ namespace _06.Carousel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 10, 20, 30, 40, 50 };
+            int n = int.Parse(Console.ReadLine());
+
+            Carousel(arr, n);
+        }
+
+        static void Carousel(int[] arr, int n)
+        {
+            int temp = arr[0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            Console.WriteLine(string.Join(' ', arr));
         }
     }
 }
