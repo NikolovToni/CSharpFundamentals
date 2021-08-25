bac64584b842b393ebfaa79a9c635865b15a5a00
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-25 16:12:43 +0300

    Create AddToArray

---
+++ TeachMeCSharp/05.Methods/07.AddToArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.AddToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
